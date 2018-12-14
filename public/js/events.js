3f8ead549e37169aa87c0d6ef0d471d535a11fae
kingiw <kingiw@hotmail.com>
2018-12-15 01:14:39 +0800

    update sign in sign up ui

--- public/js/events.js
+++ public/js/events.js
@@ -4,11 +4,49 @@ $(document).ready(function() {
 })
 
 
-$(document).ready( function() {
+$('#loginbtn').click(function() {
+    let username = $("input[name='username']").val();
+    let password = $("input[name='password']").val();
+    $.ajax({
+        type: 'POST',
+        url: '/signin',
+        data: {
+            username: username,
+            password: password
+        },
+        success: function(data) {
+            if (data.success == 0) {
+                location.href = "/"
+            } else {
+                $(".error.message").css('display', 'block');
+                $('.error.message').text(data.msg);
+            }
+        },
+        dataType: "json",
+    });
+})
 
-});
-
-
+$('#signupbtn').click(function() {
+    let username = $("input[name='username']").val();
+    let password = $("input[name='password']").val();
+    $.ajax({
+        type: 'POST',
+        url: '/signup',
+        data: {
+            username: username,
+            password: password
+        },
+        success: function(data) {
+            if (data.success == 0) {
+                location.href = "/signin"
+            } else {
+                $(".error.message").css('display', 'block');
+                $('.error.message').text(data.msg);
+            }
+        },
+        dataType: "json",
+    });
+})
 
 $('#falseinput').click(function(){
     $("#fileinput").click();
